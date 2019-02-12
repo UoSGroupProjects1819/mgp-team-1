3b3c4073553f6ad51d8be72390b9955390a4b182
Ben Carter <32357076+Ellemelmarta@users.noreply.github.com>
2019-02-12 15:46:37 +0000

    Enemies can shoot, Camera movement
    +Enemies all have a bullet pool to shoot you with which are setactive when needed or not needed
    +camera uses cinemachine and is bound to the "background so cant go out of it

---
+++ Unity/UnityProject/Assets/Scripts/Bullet.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletMoveSpeed;
+
+    private Rigidbody2D bulletRB;
+
+    private GameObject target;
+    private Vector2 moveDirection;
+
+    private bool initialised = false;
+    private bool transformRun = false;
+
+
+    public float timer;
+    private float privTimer;
+
+
+    void Start()
+    {
+        privTimer = timer;
+        bulletRB = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    void Update()
+    {
+        initialised = true;
+
+        if (initialised == true && transformRun == false)
+        {
+            transformRun = true;
+            TransformBullet();
+        }
+
+        privTimer -= Time.deltaTime;
+
+        if (timer <= 0.0f)
+        {
+            initialised = false;
+            transformRun = false;
+            privTimer = timer;
+            gameObject.SetActive(false);
+        }
+    }
+
+
+    void TransformBullet()
+    {
+        moveDirection = (target.transform.position - transform.position).normalized * bulletMoveSpeed;
+        bulletRB.velocity = new Vector2(moveDirection.x, moveDirection.y);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == target.name || other.tag == "Ground")
+        {
+            if (other.name == target.name)
+            {
+                //hurt the player
+            }
+
+            initialised = false;
+            transformRun = false;
+            privTimer = timer;
+            gameObject.SetActive(false);
+        }
+    }
+}
