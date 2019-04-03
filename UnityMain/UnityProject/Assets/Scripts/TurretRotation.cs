418b3e3e8809f328e089f807cfd1f22238771395
UoSNickBowen <nickjbowen@btinternet.com>
2019-04-03 10:43:21 +0100

    Sprite Additions

---
+++ UnityMain/UnityProject/Assets/Scripts/TurretRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretRotation : MonoBehaviour
+{
+    /*
+    public float speed = 5f;
+    public Transform target;
+
+
+    void Update()
+    {
+        Vector2 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+    }
+    */
+}
