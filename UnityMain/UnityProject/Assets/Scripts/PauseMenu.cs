0d35b44436405806f410a65d7d9f950e2e6c7eb4
gilesm <196148@uos.ac.uk>
2019-05-08 11:05:26 +0100

    Pause and Main Menu

---
+++ UnityMain/UnityProject/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static bool Paused = false;
+    public GameObject CanvasUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad7)) {
+            if (Paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+
+    public void Resume() {
+        CanvasUI.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
+    public void Pause() {
+        CanvasUI.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+    
+}
---
+++ UnityMain/UnityProject/Assets/Scripts/StartGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void NewGame() {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+}
